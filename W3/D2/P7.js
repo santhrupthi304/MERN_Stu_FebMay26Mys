e875cdfef06a4b1b2ed809876888a720472c76d6
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-03 15:16:07 +0530

    Arrow function

---
+++ W3/D2/P7.js
@@ -0,0 +1,18 @@
+// Arrow function
+
+(args) =>{
+    // body of the function
+}
+
+// Add two number
+const Sum = (a,b) => {
+    return a+b;
+}
+console.log("3+5=",Sum(3,5))
+
+// Single line return / implicit return
+const square = x => x*x;
+console.log("Square of 44: ", square(44));
+
+const sayHello = () => console.log("Hello grom arrow function");
+sayHello();
