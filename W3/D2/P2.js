aac12c19801a01903421b5f23322ae5be349e7f8
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-03 12:25:06 +0530

    Comparison Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+//  Comparison Operators
+
+console.log("Comparison Operators");
+let x=5, y='5';
+console.log("x==y: ",x==y); //Loose equality
+console.log("x===y: ",x===y); //Strict equality
+console.log("x!=y: ",x!=y); //Loose inequality
+console.log("x!==y: ",x!==y); //Strict inequality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
