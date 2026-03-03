8d7de09c54e091aaac488dab054e34e1349572fa
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-03 12:25:48 +0530

    Logical Operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,7 @@
+// Logical Operators
+
+console.log("Logical Operators");
+let a = true, b = false;
+console.log("a&&b: ",a&&b); // LOgical AND
+console.log("a||b: ",a||b);  // OR
+console.log("!a: ",!a);  // NOT(complements the value)
