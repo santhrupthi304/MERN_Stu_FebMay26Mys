28886d9179836cddbc62d5af4c11ec6582f76d70
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-03 12:24:11 +0530

    Arithemetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,15 @@
+//  Arithemetic Operators
+console.log("Arithmetic Operators");
+const a=2, b=3;
+let c=0;
+console.log("a+b: ",a+b);  //add
+console.log("a-b: ",a-b);  //sub
+console.log("a*b: ",a*b);  //multiple
+console.log("a/b: ",a/b);  //div
+console.log("a%b: ",a%b); //Modules
+console.log("a**b: ",a**b); //Exponential
+console.log("a++: ",a++);  //post-increment
+console.log("++a: ",++a);   //pre-increment
+console.log("a--: ",a--);  //post-decrement
+console.log("--a: ",--a);  // pre- decrement
+// const a=2, b=3;
