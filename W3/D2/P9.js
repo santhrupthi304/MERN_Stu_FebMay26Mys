37dc01f4b99d0d9e584541dcce7181e104d69b58
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-03 15:23:49 +0530

    Immediately Invoked Function Expression (IIFE)

---
+++ W3/D2/P9.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function(){
+    console.log("Basic IIFE executed immediately");
+})();
+// with parameter
+(function(appName,version){
+    console.log("App: ",appName,"Version: ",version);
+})("NodeJS","V22.22.0");
+// with return value
+const result = (function(){
+    const a = 10, b = 20;
+    return a+b;
+})();
+console.log("Sum is",result);
