9c218bfc905caab063102ae9a1994c92a8186380
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-04 13:31:23 +0530

    while and do-while  loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,17 @@
+// while loop
+
+console.log("while loop");
+
+let count = 0;
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
+
+// do-while loop
+
+let value = 0;
+do{
+    console.log("Value:",value);
+    value++;
+}while(value<3);
