1f316b12ea3113948884347b434437b8688315d4
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-04 14:36:43 +0530

    Object Iteration

---
+++ W3/D3/P7.js
@@ -0,0 +1,15 @@
+// Object Iteration
+
+const book = {
+    title: "35 for newbies",
+    author: "____",
+    year: 2026
+};
+
+for(let key in book){
+    // console.log(key,":",book[key]);
+}
+console.log("Keys",Object.keys(book));
+console.log("Values",Object.values(book));
+console.log("Entries",Object.entries(book));
+
