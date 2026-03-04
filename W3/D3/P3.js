6209382816addd000e6b6d1786c315d33b4c3c1a
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-04 13:33:54 +0530

    Break and Continue loop

---
+++ W3/D3/P3.js
@@ -0,0 +1,18 @@
+// Break
+
+// for(let i=0;i<5;i++){        
+//     if(i===4){
+//         console.log("i",i);
+//         break;
+//     }
+// }
+
+// Continue
+for(let i=0;i<5;i++){        
+    if(i===2){
+        // console.log("i",i);
+        continue;
+    }
+    console.log("i",i);
+}
+
