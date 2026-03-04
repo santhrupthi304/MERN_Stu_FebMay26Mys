df5b00732ddc87761642cc22bf50fcdfbbe95577
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-04 14:14:39 +0530

    //  Nested Objects and methods

---
+++ W3/D3/P6.js
@@ -0,0 +1,22 @@
+//  Nested Objects and methods 
+
+const student = {
+    firstName: "Santhu",
+    lastName: "Gowda",
+    scores:{
+        math:80,
+        science:48,
+        social:89
+    },
+    hobbies:["reading","drawing"],
+    fullname: function(){
+        return this.firstName + " "+this.lastName;
+    },
+    greet(){
+        console.log("Hi, ",this.fullname());
+    },
+};
+// console.log("Student",student);
+// console.log(student);
+// console.log(student.scores.math);
+console.log(student.fullname());
