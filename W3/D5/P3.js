004a0ab835b03229468d812fe323528edf54787e
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-09 09:03:41 +0530

    Mouseover event

---
+++ W3/D5/P3.js
@@ -0,0 +1,13 @@
+// Mouse events
+
+const hoverBox = document.getElementById("hoverBox");
+
+hoverBox.addEventListener("mouseover",function(){
+    hoverBox.textContent = "Mouse is over me";
+    hoverBox.style.backgroundColor = "lightyellow";
+});
+
+hoverBox.addEventListener("mouseout",function(){
+    hoverBox.textContent = "Mouse is over me";
+    hoverBox.style.backgroundColor = "";
+});
