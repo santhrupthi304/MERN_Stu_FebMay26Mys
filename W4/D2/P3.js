96124e0de3cedc5ebb638e5214c9f6ac399c2a1c
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-10 11:54:09 +0530

    insertAdjacentHTML

---
+++ W4/D2/P3.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container");
+// position:
+// beforebegin:
+// afterbegin:
+// beforeend:
+// afterend:
+
+document.getElementById("btn").addEventListener("click",function(){
+    container.insertAdjacentHTML("beforebegin","<p>Dynamically inserted</p>");
+});
