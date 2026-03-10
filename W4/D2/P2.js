624380a4f9187cfb1b97436823747e98f8bac7f8
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-10 10:36:32 +0530

    Create elements

---
+++ W4/D2/P2.js
@@ -0,0 +1,21 @@
+// CreateElement() & appendChild()
+
+const list = document.getElementById("list");
+let counter = 1;
+
+document.getElementById("addBtn").addEventListener("click",function(){
+    errMessage.textContent=" ";
+    const li = document.createElement("li");
+    li.textContent = "Item " + counter++;
+    list.appendChild(li);
+});
+
+document.getElementById("rmBtn").addEventListener("click",function(){
+    if (list.lastElementChild){
+        list.removeChild(list.lastElementChild);
+    }
+    else{
+        // console.log("There is no item to be deleted");
+        errMessage.textContent="No items to delete";
+    }
+});
