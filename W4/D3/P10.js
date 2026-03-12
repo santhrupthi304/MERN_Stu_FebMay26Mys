5025525671c1fe4e8004d1087b35982af8fb69c9
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-12 11:46:27 +0530

    async Fetch Users

---
+++ W4/D3/P10.js
@@ -0,0 +1,19 @@
+const asyncFetchBtn = document.getElementById("asyncFetchBtn");
+const output = document.getElementById("output");
+
+asyncFetchBtn.addEventListener("click", async function () {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!response.ok) throw new Error("http Error:", response.status);
+
+        const users = await response.json();
+        console.log(users);
+        const firstThree = users.slice(0,3);
+        output.textContent = JSON.stringify(firstThree, null, 2);
+    }
+    catch (error) {
+        output.textContent = "Error:" + error.message;
+    }
+
+
+})
