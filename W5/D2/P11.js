592f5b2ff48082757448db13fe8c01b8903c0036
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-17 16:18:21 +0530

    Chaining Promises with returned promises

---
+++ W5/D2/P11.js
@@ -0,0 +1,33 @@
+// Chaining Promises with returned promises
+
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+        },500);
+    });
+}
+
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id: orderId,
+                product: "Laptop",
+                quantity: 2
+            });
+        },700);
+    });
+}
+
+getOrderId()
+.then(function(orderId){
+    console.log("Order Id received: ",orderId);
+    return getOrderDetails(orderId);
+})
+
+.then(function(OrderDetails){
+    console.log("Order details loaded.");
+    console.log("Product:",OrderDetails.product);
+    console.log("Quantity:",OrderDetails.quantity);
+});
