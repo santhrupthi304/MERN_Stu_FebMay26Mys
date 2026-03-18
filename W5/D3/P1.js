72ed6948a82fc5c3d8ac5809f3ffc9c9441de2f6
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-18 10:02:13 +0530

    Introduction to Async/Await

---
+++ W5/D3/P1.js
@@ -0,0 +1,16 @@
+// Introduction to Async/Await
+function getMessage(){
+    return new Promise(function(resolve){
+        setTimeout(() => {
+            resolve("Async/Await makes promise based code easier to read");
+        },1000);
+    });
+}
+
+async function showMessage(){
+    console.log("Loading message....");
+    const message = await getMessage();
+    console.log(message);
+}
+
+showMessage();
