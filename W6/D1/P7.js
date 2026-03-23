4110fc0ee872a2d287ce0aa827b78a3b43e205a1
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-23 17:24:19 +0530

    Understanding Path module and JSON module

---
+++ W6/D1/P7.js
@@ -0,0 +1,12 @@
+//  Understanding Path module and JSON module
+
+const path = require("path");
+// JSON is loaded as a normal JS object in CommonJS
+const appConfig = require("./support/app-config.json");
+
+console.log("__dirname",__dirname);
+console.log("__filename",__filename);
+
+console.log("Application name: ",appConfig.appName);
+console.log("Environment: ",appConfig.environment);
+console.log("Features: ",appConfig.features.join(", "));
