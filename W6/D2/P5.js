06f527b0920f54518ad6445f77f70ec5570f70d3
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-24 16:18:13 +0530

    Microtasks and Macrotasks in NodeJS

---
+++ W6/D2/P5.js
@@ -0,0 +1,15 @@
+// Microtasks and Macrotasks in NodeJS
+console.log("1. Synchronous start.");
+
+// promise.resolve(...).then(....) schedule a microtask
+Promise.resolve().then(function(){
+    console.log("3. Promise microtask executed.");
+});
+
+// setTimeout(...,0) schedules task for a later time.
+// Even with 0 delay, it doesn't interrupt current sync code
+setTimeout(() => {
+    console.log("4. Timer callback executed.");
+},0);
+
+console.log("2. Synchronous end.");
