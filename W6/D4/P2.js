fbc517ca2d289bb68e89ece5c5470a84c51361f7
santhrupthi304 <santhrupthis997@gmail.com>
2026-03-26 16:02:14 +0530

    Creating a simple HTTP server

---
+++ W6/D4/P2.js
@@ -0,0 +1,25 @@
+// Creating a simple HTTP server
+
+const http = require("http");
+
+// createServer(): creates a HTTP server instance
+// Accepts a callback with two important objects:
+// 1. req : incoming request details
+// 2. res : outgoing response control
+
+const server = http.createServer(function(req,res){
+    // writeHead() sets the reponse status code and headers
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    // end() sends the response body and closes the response
+    res.end("Hello from NodeJS HTTP server.");
+});
+
+// listen() binds the server to a port and starts accepting requests
+server.listen(3000,function(){
+    console.log("server is running at http://localhost:3000");
+});
+
+// port nos:
+// 0-1023: system ports
+// 1024-49151: "Registered ports"
+// 27017: mangoDB
