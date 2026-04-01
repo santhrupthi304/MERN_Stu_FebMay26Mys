4eb4ffd1fa42bb0b0f900126f8074bab9ba52f4b
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-01 10:31:30 +0530

    Handling Asynchronous errors with async/await

---
+++ W7/D3/P3.js
@@ -0,0 +1,30 @@
+// Handling Asynchronous errors with async/await
+const express = require("express");
+const app = express();
+
+function loadUserProfile(){
+    return Promise.reject(new Error("User profile can't be loaded"));
+}
+
+app.get("/async-fail",async function(req,res,next){
+    try{
+        const profile = await loadUserProfile();
+        res.json(profile);
+    }
+    catch(error){
+        next(error);
+    }
+});
+
+// Centeralized error handling middleware
+app.use(function(error,req,res,next){
+    res.status(400).json({
+        success:false,
+        message:"Async/Await error handled centrally.",
+        // OriginalMessage:error.message
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
