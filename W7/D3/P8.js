d985be9da295defea0de02f959edb847d3317e0e
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-02 12:43:49 +0530

    JWT flow with login, refresh style logic and secure verification

--- W7/D3/P8.js
+++ W7/D3/P8.js
@@ -115,3 +115,4 @@ app.listen(4000,function(){
 
 //curl -X POST http://localhost:4000/login -H "Content-Type:application/json" -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}"
 //curl -X POST http://localhost:4000/login -H "Content-Type:application/json" -d "{\"refreshToken\":\"refresh token link\"}"
+// curl http://localhost:4000/me -H "Authorization:Bearer accessTokenlink"
