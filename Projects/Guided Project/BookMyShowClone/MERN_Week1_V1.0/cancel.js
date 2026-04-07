9855ce49c9e7522d9c771890c940c01f4f403cad
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-07 09:42:38 +0530

    To cancel the existing booking if exists

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
@@ -1,4 +1,4 @@
-// cancel.js
+//cancel.js
 // To cancel the existing booking if exists
 const bookingEmitter = require("./events");
 const {getCurrentBooking,clearCurrentBooking} = require("./booking");
@@ -6,23 +6,25 @@ const {getCurrentBooking,clearCurrentBooking} = require("./booking");
 function cancelBooking(movies){
     const booking = getCurrentBooking();
 
-    if(!booking){
+    if (!booking) {
         bookingEmitter.emit("bookingFailed","No booking found to cancel.");
         return null;
     }
-    const Movie = movies.find((m)=>m.id === booking.movieId);
-    if(!movie){
+
+    const movie = movies.find((m)=>m.id === booking.movieId);
+    if (!movie) {
         bookingEmitter.emit("bookingFailed","Movie data not found while cancelling booking.");
         return null;
     }
-    const showtime = movies.showtimes.find(show=>show.time.toLowerCase() === booking.time.toLowerCase());
-    if(!showtime){
-        bookingEmitter.emit("bookingFailed","showtime data not found");
+
+    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase()===booking.time.toLowerCase());
+    if (!showtime) {
+        bookingEmitter.emit("bookingFailed","Showtime data not found");
         return null;
     }
 
     //restore seats
-    showtime.seatsAvailable += booking.seatCount;
+    showtime.seatsAvailable +=booking.seatCount;
 
     // clear current Booking
     clearCurrentBooking();
