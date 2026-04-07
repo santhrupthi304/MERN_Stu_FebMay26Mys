72d21c97f53f0282a50daa1e9b1e529cb9099d50
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-07 09:43:45 +0530

    This file stores the movie data used in the CLI App

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/movies.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/movies.js
@@ -1,35 +1,35 @@
 // movies.js
-// This file stores the movie data used in the CLI App.
+// This file stores the movie data used in the CLI App
 
 const movies = [
     {
-        id: 1,
-        title: "Dhurandhar-2",
+        id:1,
+        title:"Dhurandar2",
         showtimes:[
-            {time: "10:00 AM", seatsAvailable:100},
-            {time: "1:00 PM", seatsAvailable:70},
-            {time: "6:00 PM", seatsAvailable:300},
+            {time: "10:00 AM",seatsAvailable: 10},
+            {time: "1:00 PM",seatsAvailable: 7},
+            {time: "6:00 PM",seatsAvailable: 30}
         ]
     },
     {
-        id: 2,
-        title: "LoveMoktail-3",
+        id:2,
+        title:"LoveMocktail3",
         showtimes:[
-            {time: "10:00 AM", seatsAvailable:100},
-            {time: "1:00 PM", seatsAvailable:70},
-            {time: "6:00 PM", seatsAvailable:300},
+            {time: "10:00 AM",seatsAvailable: 10},
+            {time: "1:00 PM",seatsAvailable: 7},
+            {time: "6:00 PM",seatsAvailable: 30}
         ]
     },
     {
-        id: 3,
-        title: "Hayagreeva",
+        id:3,
+        title:"Hayagreeva",
         showtimes:[
-            {time: "10:00 AM", seatsAvailable:100},
-            {time: "1:00 PM", seatsAvailable:70},
-            {time: "6:00 PM", seatsAvailable:300},
+            {time: "11:00 AM",seatsAvailable: 10},
+            {time: "2:00 PM",seatsAvailable: 7},
+            {time: "7:00 PM",seatsAvailable: 30}
         ]
-    },
+    }
 ];
 
-// Export the movie data so that other files can use it.
+//Export the movie data so that other files can use it. 
 module.exports = movies;
