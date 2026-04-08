0564a1ffb1a34186a98f9d2bf8ac41021a7f46ae
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-08 10:19:44 +0530

    CustomError class created for error handling

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/utils/customError.js
@@ -0,0 +1,9 @@
+// CustomError class created for error handling
+class CustomError extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = CustomError;
