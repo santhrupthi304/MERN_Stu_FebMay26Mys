c8ba651e139ae965bd6a10bc5d8f1058482ff237
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-15 12:47:36 +0530

    Date fundamentals

---
+++ W8/D4/P1.js
@@ -0,0 +1,32 @@
+// Date fundamentals
+const mongoose = require ("mogoose");
+
+async function main(){
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/embrel");
+        console.log("Connected to MongoDB");
+        const schema = new mongoose.Schema({
+            name:String,
+            createdAt:{
+                type:Date,
+                default:Date.now
+            }
+        });
+        
+        const Model = mongoose.model('DateFund',schema);
+        await Model.deleteMany();
+
+        const doc = await Model.create({
+            name:"Test"
+        });
+        console.log("Document:",doc);
+    }
+    catch(error){
+        console.error("Error:",error.message);
+    }
+    finally{
+                await mongoose.disconnect();
+                console.log("Disconnected from DB.")
+    }
+}
+main();
