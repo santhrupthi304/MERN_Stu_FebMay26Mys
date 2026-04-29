0ce091e58cc5b24854de50b6a09de07347af2f0b
santhrupthi304 <santhrupthis997@gmail.com>
2026-04-29 14:11:40 +0530

    Props destructuring

--- W9/D2/prop-example-app/src/components/P2.jsx
+++ W9/D2/prop-example-app/src/components/P2.jsx
@@ -1,7 +1,9 @@
 // Props destructuring
+// a syntax ES^ approch, that allows us to unpack properties from props
+// prop object directly into its values
 import {React} from 'react';
 
-// 
+// Child component
 function UserProfile({username,skill}){
     return(
         <div>
