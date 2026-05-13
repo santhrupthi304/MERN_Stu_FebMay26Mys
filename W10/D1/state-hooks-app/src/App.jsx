a92711814f52d91366c44addc0e88cd129449392
santhrupthi304 <santhrupthis997@gmail.com>
2026-05-13 12:18:37 +0530

    useEffect for side effects

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { UseStateBasics } from './components/P1'
-
+import { UpDatingObjectsArraysState } from './components/P2'
+import { EventHandlingState } from './components/P3'
+import { DerivedStateCondRender } from './components/P4'
 function App() {
 
   return (
     <>
-     <UseStateBasics/>
+     {/* <UseStateBasics/> */}
+     {/* <UpDatingObjectsArraysState/> */}
+     {/* <EventHandlingState/> */}
+     <DerivedStateCondRender/>
     </>
   )
 }
